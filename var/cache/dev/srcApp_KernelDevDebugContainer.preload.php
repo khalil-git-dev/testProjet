4fc80712214c7099b75016b4905fe5b719797242
lofatou62035@gmail.com <lofatou62035@gmail.com>
2021-04-29 23:09:59 +0000

    [GCEL2]:createRole

--- var/cache/dev/srcApp_KernelDevDebugContainer.preload.php
+++ var/cache/dev/srcApp_KernelDevDebugContainer.preload.php
@@ -10,7 +10,7 @@ if (in_array(PHP_SAPI, ['cli', 'phpdbg'], true)) {
 }
 
 require dirname(__DIR__, 3).'/vendor/autoload.php';
-require __DIR__.'/ContainerM4zxx7R/srcApp_KernelDevDebugContainer.php';
+require __DIR__.'/Container3Fspxx0/srcApp_KernelDevDebugContainer.php';
 
 $classes = [];
 $classes[] = 'Symfony\Bundle\FrameworkBundle\FrameworkBundle';
@@ -32,6 +32,8 @@ $classes[] = 'Symfony\Bundle\FrameworkBundle\Controller\ControllerNameParser';
 $classes[] = 'Symfony\Bundle\FrameworkBundle\EventListener\ResolveControllerNameSubscriber';
 $classes[] = 'Symfony\Component\HttpFoundation\RequestMatcher';
 $classes[] = 'Symfony\Component\DependencyInjection\ServiceLocator';
+$classes[] = 'App\Repository\EleveRepository';
+$classes[] = 'App\Repository\RoleRepository';
 $classes[] = 'App\Repository\UserRepository';
 $classes[] = 'Doctrine\Bundle\DoctrineBundle\Dbal\ManagerRegistryAwareConnectionProvider';
 $classes[] = 'Doctrine\Bundle\DoctrineBundle\Registry';
